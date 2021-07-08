5433bf91c68e4fbd61425f8ed246c6da4bf56ff6
cdedonno <carlo.dedonno@helmholtz-muenchen.de>
2021-07-08 12:50:54 +0200

    start embedcvae arch

---
+++ tranvae/model/embedding_cvae.py
@@ -0,0 +1,519 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from torch.distributions import Normal, kl_divergence
+from typing import Optional
+
+from scarches.models.trvae._utils import one_hot_encoder
+from scarches.models.trvae.losses import mse, zinb, nb
+
+class EmbedCVAE(nn.Module):
+    def __init__(
+        self,
+        input_dim,
+        hidden_layer_sizes,
+        conditions,
+        inject_condition,
+        latent_dim,
+        embedding_dim,
+        recon_loss,
+        dr_rate, 
+        beta,
+        use_bn,
+    ):
+    super(EmbedCVAE, self).__init__()
+
+    self.input_dim = input_dim
+    self.latent_dim = latent_dim
+    self.embedding_dim = embedding_dim
+    self.n_conditions = len(conditions)
+    self.conditions = conditions
+    self.inject_condition = inject_condition
+    self.use_bn = use_bn
+    self.recon_loss = recon_loss
+    self.hidden_layer_sizes = hidden_layer_sizes
+
+    encoder_layer_sizes = self.hidden_layer_sizes.copy()
+    encoder_layer_sizes.insert(0, self.input_dim)
+    decoder_layer_sizes = self.hidden_layer_sizes.copy()
+    decoder_layer_sizes.reverse()
+    decoder_layer_sizes.append(self.input_dim)
+
+    self.embedding = nn.Embedding(
+        self.n_conditions,
+        self.embedding_dim,
+    )
+    self.encoder = Encoder(
+        encoder_layer_sizes,
+        self.latent_dim,
+        self.use_bn,
+        self.use_ln,
+        self.use_dr,
+        self.dr_rate,
+        self.n_conditions
+    )
+    self.decoder = Decoder(
+        decoder_layer_sizes,
+        self.latent_dim,
+        self.recon_loss,
+        self.use_bn,
+        self.use_ln,
+        self.use_dr,
+        self.dr_rate,
+        self.n_conditions
+    )
+
+    def forward(
+        self,
+        x=None,
+        batch=None,
+        sizefactor=None,
+        labeled=None,
+    ):
+        x_log = torch.log(1 + x)
+        if self.recon_loss == 'mse':
+            x_log = x
+        z1_mean, z1_log_var = self.encoder(x_log, batch)
+        latent = self.sampling(z1_mean, z1_log_var)
+        outputs = self.decoder(z1, batch)
+
+        if self.recon_loss == "mse":
+            recon_x, y1 = outputs
+            recon_loss = mse(recon_x, x_log).sum(dim=-1).mean()
+        elif self.recon_loss == "zinb":
+            dec_mean_gamma, dec_dropout, y1 = outputs
+            size_factor_view = (
+                sizefactor
+                .unsqueeze(1)
+                .expand(
+                    dec_mean_gamma.size(0), 
+                    dec_mean_gamma.size(1)
+                )
+            )
+            dec_mean = dec_mean_gamma * size_factor_view
+            dispersion = F.linear(
+                one_hot_encoder(batch, self.n_conditions), 
+                self.theta
+            )
+            dispersion = torch.exp(dispersion)
+            recon_loss = -zinb(
+                x=x, 
+                mu=dec_mean, 
+                theta=dispersion, 
+                pi=dec_dropout
+            ).sum(dim=-1).mean()
+        elif self.recon_loss == "nb":
+            dec_mean_gamma, y1 = outputs
+            size_factor_view = (
+                sizefactor
+                .unsqueeze(1)
+                .expand(
+                    dec_mean_gamma.size(0), 
+                    dec_mean_gamma.size(1)
+                )
+            )
+            dec_mean = dec_mean_gamma * size_factor_view
+            dispersion = F.linear(
+                one_hot_encoder(batch, self.n_conditions), 
+                self.theta
+            )
+            dispersion = torch.exp(dispersion)
+            recon_loss = -nb(
+                x=x, 
+                mu=dec_mean, 
+                theta=dispersion
+            ).sum(dim=-1).mean()
+
+    def sampling(self, mu, log_var):
+        """Samples from standard Normal distribution and applies re-parametrization trick.
+        It is actually sampling from latent space distributions with N(mu, var), computed by encoder.
+        Parameters
+        ----------
+        mu: torch.Tensor
+                Torch Tensor of Means.
+        log_var: torch.Tensor
+                Torch Tensor of log. variances.
+        Returns
+        -------
+        Torch Tensor of sampled data.
+        """
+        var = torch.exp(log_var) + 1e-4
+        return Normal(mu, var.sqrt()).rsample()
+
+class Encoder(nn.Module):
+    """ScArches Encoder class. Constructs the encoder sub-network of TRVAE and CVAE. It will transform primary space
+       input to means and log. variances of latent space with n_dimensions = z_dimension.
+       Parameters
+       ----------
+       layer_sizes: List
+            List of first and hidden layer sizes
+       latent_dim: Integer
+            Bottleneck layer (z)  size.
+       use_bn: Boolean
+            If `True` batch normalization will be applied to layers.
+       use_ln: Boolean
+            If `True` layer normalization will be applied to layers.
+       use_dr: Boolean
+            If `True` dropout will applied to layers.
+       dr_rate: Float
+            Dropput rate applied to all layers, if `dr_rate`==0 no dropput will be applied.
+       num_classes: Integer
+            Number of classes (conditions) the data contain. if `None` the model will be a normal VAE instead of
+            conditional VAE.
+    """
+    def __init__(
+        self,
+        layer_sizes: list,
+        latent_dim: int,
+        use_bn: bool,
+        use_ln: bool,
+        use_dr: bool,
+        dr_rate: float,
+        num_classes: int = None
+    ):
+        super().__init__()
+
+        self.n_classes = 0
+        if num_classes is not None:
+            self.n_classes = num_classes
+        self.FC = None
+
+        if len(layer_sizes) > 1:
+            print("Encoder Architecture:")
+            self.FC = nn.Sequential()
+            for i, (in_size, out_size) in enumerate(
+                zip(layer_sizes[:-1], layer_sizes[1:])
+                ):
+                if i == 0:
+                    print(
+                        "\tInput Layer in, out and cond:", 
+                        in_size, 
+                        out_size, 
+                        self.n_classes
+                    )
+                    (
+                        self
+                        .FC
+                        .add_module(
+                            name="L{:d}".format(i), 
+                            module=CondLayers(
+                                in_size,
+                                out_size,
+                                self.n_classes,
+                                bias=True
+                            )
+                        )
+                    )
+
+                else:
+                    print("\tHidden Layer", i, "in/out:", in_size, out_size)
+                    (
+                        self
+                        .FC
+                        .add_module(
+                            name="L{:d}".format(i), 
+                            module=nn.Linear(
+                                in_size, 
+                                out_size, 
+                                bias=True
+                            )
+                        )
+                    )
+                if use_bn:
+                    (
+                        self
+                        .FC
+                        .add_module(
+                            "N{:d}".format(i), 
+                            module=nn.BatchNorm1d(out_size, affine=True)
+                        )
+                    )
+                elif use_ln:
+                    (
+                        self
+                        .FC
+                        .add_module(
+                            "N{:d}".format(i), 
+                            module=nn.LayerNorm(
+                                out_size, 
+                                elementwise_affine=False
+                            )
+                        )
+                    )
+                self.FC.add_module(
+                    name="A{:d}".format(i), 
+                    module=nn.ReLU()
+                )
+                if use_dr:
+                    self.FC.add_module(
+                        name="D{:d}".format(i), 
+                        module=nn.Dropout(p=dr_rate)
+                    )
+
+        print("\tMean/Var Layer in/out:", layer_sizes[-1], latent_dim)
+        self.mean_encoder = nn.Linear(layer_sizes[-1], latent_dim)
+        self.log_var_encoder = nn.Linear(layer_sizes[-1], latent_dim)
+
+    def forward(self, x, batch=None):
+        if batch is not None:
+            batch = one_hot_encoder(batch, n_cls=self.n_classes)
+            x = torch.cat((x, batch), dim=-1)
+        if self.FC is not None:
+            x = self.FC(x)
+        means = self.mean_encoder(x)
+        log_vars = self.log_var_encoder(x)
+        return means, log_vars
+
+
+class Decoder(nn.Module):
+    """ScArches Decoder class. Constructs the decoder sub-network of TRVAE or CVAE networks. It will transform the
+       constructed latent space to the previous space of data with n_dimensions = x_dimension.
+       Parameters
+       ----------
+       layer_sizes: List
+            List of hidden and last layer sizes
+       latent_dim: Integer
+            Bottleneck layer (z)  size.
+       recon_loss: String
+            Definition of Reconstruction-Loss-Method, 'mse', 'nb' or 'zinb'.
+       use_bn: Boolean
+            If `True` batch normalization will be applied to layers.
+       use_ln: Boolean
+            If `True` layer normalization will be applied to layers.
+       use_dr: Boolean
+            If `True` dropout will applied to layers.
+       dr_rate: Float
+            Dropput rate applied to all layers, if `dr_rate`==0 no dropput will be applied.
+       num_classes: Integer
+            Number of classes (conditions) the data contain. if `None` the model will be a normal VAE instead of
+            conditional VAE.
+    """
+    def __init__(
+        self,
+        layer_sizes: list,
+        latent_dim: int,
+        recon_loss: str,
+        use_bn: bool,
+        use_ln: bool,
+        use_dr: bool,
+        dr_rate: float,
+        num_classes: int = None
+    ):
+        super().__init__()
+        self.use_dr = use_dr
+        self.recon_loss = recon_loss
+        self.n_classes = 0
+        if num_classes is not None:
+            self.n_classes = num_classes
+        layer_sizes = [latent_dim] + layer_sizes
+        print("Decoder Architecture:")
+        # Create first Decoder layer
+        self.FirstL = nn.Sequential()
+        print(
+            "\tFirst Layer in, out and cond: ", 
+            layer_sizes[0], 
+            layer_sizes[1], 
+            self.n_classes
+        )
+
+        (
+            self
+            .FirstL
+            .add_module(
+                name="L0", 
+                module=CondLayers(
+                    layer_sizes[0],
+                    layer_sizes[1], 
+                    self.n_classes, 
+                    bias=False
+                )
+            )
+        )
+        if use_bn:
+            (
+                self
+                .FirstL
+                .add_module(
+                    "N0", 
+                    module=nn.BatchNorm1d(
+                        layer_sizes[1], 
+                        affine=True
+                    )
+                )
+            )
+        elif use_ln:
+            (
+                self
+                .FirstL
+                .add_module(
+                    "N0", 
+                    module=nn.LayerNorm(
+                        layer_sizes[1],
+                        elementwise_affine=False
+                    )
+                )
+            )
+        (
+            self
+            .FirstL
+            .add_module(
+                name="A0", 
+                module=nn.ReLU()
+            )
+        )
+        if self.use_dr:
+            self.FirstL.add_module(name="D0", module=nn.Dropout(p=dr_rate))
+
+        # Create all Decoder hidden layers
+        if len(layer_sizes) > 2:
+            self.HiddenL = nn.Sequential()
+            for i, (in_size, out_size) in enumerate(
+                zip(
+                    layer_sizes[1:-1], 
+                    layer_sizes[2:]
+                )
+            ):
+                if i+3 < len(layer_sizes):
+                    print("\tHidden Layer", i+1, "in/out:", in_size, out_size)
+                    (
+                        self
+                        .HiddenL
+                        .add_module(
+                            name="L{:d}".format(i+1), 
+                            module=nn.Linear(
+                                in_size, 
+                                out_size, 
+                                bias=False
+                            )
+                        )
+                    )
+                    if use_bn:
+                        (
+                            self
+                            .HiddenL
+                            .add_module(
+                                "N{:d}".format(i+1), 
+                                module=nn.BatchNorm1d(
+                                    out_size, 
+                                    affine=True
+                                )
+                            )
+                        )
+                    elif use_ln:
+                        (
+                            self
+                            .HiddenL
+                            .add_module(
+                                "N{:d}".format(i + 1), 
+                                module=nn.LayerNorm(
+                                    out_size, 
+                                    elementwise_affine=False
+                                )
+                            )
+                    (
+                        self
+                        .HiddenL
+                        .add_module(
+                            name="A{:d}".format(i+1), 
+                            module=nn.ReLU()
+                        )
+                    )
+                    if self.use_dr:
+                        (
+                            self
+                            .HiddenL
+                            .add_module(
+                                name="D{:d}".format(i+1), 
+                                module=nn.Dropout(p=dr_rate)
+                            )
+                        )
+        else:
+            self.HiddenL = None
+
+        # Create Output Layers
+        print("\tOutput Layer in/out: ", layer_sizes[-2], layer_sizes[-1], "\n")
+        if self.recon_loss == "mse":
+            self.recon_decoder = nn.Sequential(
+                nn.Linear(
+                    layer_sizes[-2], 
+                    layer_sizes[-1]
+                ), 
+                nn.ReLU()
+            )
+        if self.recon_loss == "zinb":
+            # mean gamma
+            self.mean_decoder = nn.Sequential(
+                nn.Linear(
+                    layer_sizes[-2], 
+                    layer_sizes[-1]
+                ), 
+                nn.Softmax(dim=-1)
+            )
+            # dropout
+            self.dropout_decoder = nn.Linear(
+                layer_sizes[-2], 
+                layer_sizes[-1]
+            )
+        if self.recon_loss == "nb":
+            # mean gamma
+            self.mean_decoder = nn.Sequential(
+                nn.Linear(
+                    layer_sizes[-2], 
+                    layer_sizes[-1]
+                ), 
+                nn.Softmax(dim=-1)
+            )
+
+    def forward(self, z, batch=None):
+        # Add Condition Labels to Decoder Input
+        if batch is not None:
+            batch = one_hot_encoder(batch, n_cls=self.n_classes)
+            z_cat = torch.cat((z, batch), dim=-1)
+            dec_latent = self.FirstL(z_cat)
+        else:
+            dec_latent = self.FirstL(z)
+
+        # Compute Hidden Output
+        if self.HiddenL is not None:
+            x = self.HiddenL(dec_latent)
+        else:
+            x = dec_latent
+
+        # Compute Decoder Output
+        if self.recon_loss == "mse":
+            recon_x = self.recon_decoder(x)
+            return recon_x, dec_latent
+        elif self.recon_loss == "zinb":
+            dec_mean_gamma = self.mean_decoder(x)
+            dec_dropout = self.dropout_decoder(x)
+            return dec_mean_gamma, dec_dropout, dec_latent
+        elif self.recon_loss == "nb":
+            dec_mean_gamma = self.mean_decoder(x)
+            return dec_mean_gamma, dec_latent
+
+class CondLayers(nn.Module):
+    def __init__(
+            self,
+            n_in: int,
+            n_out: int,
+            n_cond: int,
+            bias: bool,
+    ):
+        super().__init__()
+        self.n_cond = n_cond
+        self.expr_L = nn.Linear(n_in, n_out, bias=bias)
+        if self.n_cond != 0:
+            self.cond_L = nn.Linear(self.n_cond, n_out, bias=False)
+
+    def forward(self, x: torch.Tensor):
+        if self.n_cond == 0:
+            out = self.expr_L(x)
+        else:
+            expr, cond = torch.split(
+                x, 
+                [x.shape[1] - self.n_cond, self.n_cond],
+                dim=1
+            )
+            out = self.expr_L(expr) + self.cond_L(cond)
+        return out
