9607eb720a0623bb0f7c88ea4bd7db0e0c94ab93
cdedonno <carlo.dedonno@helmholtz-muenchen.de>
2021-10-15 12:45:59 +0200

    noverify

--- lataq/models/__init__.py
+++ lataq/models/__init__.py
@@ -1,5 +1,4 @@
 from .lataq_model import LATAQ
-
-#these need to come later to avoid circular import
+# these need to come later to avoid circular import
 from .embedcvae.embedcvae_model import EMBEDCVAE
 from .tranvae.tranvae_model import TRANVAE
