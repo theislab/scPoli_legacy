cf73d302b416e4da54a40b9af307b3ffc4d7aada
cdedonno <carlo.dedonno@helmholtz-muenchen.de>
2021-10-15 12:46:58 +0200

    fix circular import

--- lataq/models/__init__.py
+++ lataq/models/__init__.py
@@ -1,4 +1,4 @@
-from .lataq_model import LATAQ
 # these need to come later to avoid circular import
 from .embedcvae.embedcvae_model import EMBEDCVAE
+from .lataq_model import LATAQ
 from .tranvae.tranvae_model import TRANVAE
